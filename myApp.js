b6fe811dfc92d09e4ef28b871fa71af0e5c0ece4
Taiwo Oladipupo <telleosis@gmail.com>
2022-12-16 21:38:47 +0100

    Updated myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,28 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
 require("dotenv").config();
+
+const mySecret = process.env["MESSAGE_STYLE"];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use("/", (req, res, next) => {
+  console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
+  next();
+});
+
+app.use("/public", (req, res, next) => {
+  console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
+  next();
+});
+
+app.use("/json", (req, res, next) => {
+  console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
+  next();
+});
 
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -9,26 +31,42 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  if (process.env.MESSAGE_STYLE === "uppercase") {
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
     return res.json({ message: "HELLO JSON" });
   } else {
     return res.json({ message: "Hello json" });
   }
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
 app.get("/:word/echo", (req, res) => {
   const word = req.params.word;
   res.send({ echo: word });
 });
 
-app.get("/name", (req, res) => {
-  let firstName = req.query.first;
-  let lastName = req.query.last;
+app.post("/name", (req, res) => {
+  let firstName = req.body.first;
+  let lastName = req.body.last;
 
   res.json({ name: `${firstName} ${lastName}` });
-});
+}),
+  app.get("/name", (req, res) => {
+    let firstName = req.query.first;
+    let lastName = req.query.last;
 
-/** 
+    res.json({ name: `${firstName} ${lastName}` });
+  }),
+  /** 
  * Send back a string
  * /
 // app.get("/", (req, res) => {
@@ -45,67 +83,97 @@ app.get("/name", (req, res) => {
 /**
  * Send back a JSON
  */
-// app.get("/json", (req, res) => {
-//   const simpleJSONObj = { message: "Hello json" };
-//   res.json(simpleJSONObj);
-// });
+  // app.get("/json", (req, res) => {
+  //   const simpleJSONObj = { message: "Hello json" };
+  //   res.json(simpleJSONObj);
+  // });
 
-/**
- * Use a .env variable
- * Must install "dotenv": "^16.0.1", in package.json
- * Must import require("dotenv").config();
- */
-// app.get("/json", (req, res) => {
-//   if (process.env.MESSAGE_STYLE === "uppercase") {
-//     return res.json({ "message": "HELLO JSON" });
-//   } else {
-//     return res.json({ "message": "Hello json" });
-//   }
-// });
+  /**
+   * Use a .env variable
+   * Must install "dotenv": "^16.0.1", in package.json
+   * Must import require("dotenv").config();
+   */
+  // app.get("/json", (req, res) => {
+  //   if (process.env.MESSAGE_STYLE === "uppercase") {
+  //     return res.json({ "message": "HELLO JSON" });
+  //   } else {
+  //     return res.json({ "message": "Hello json" });
+  //   }
+  // });
 
-/**
- * Implementing a Root-Level Request Logger Middleware
- * The Middleware must be placed before the HTTP verbs, or
- * HTTP functions
- */
-// app.use("/", (req, res, next) => {
-//     console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
-//     next();
-//   });
+  /**
+   * Use body-parser to Parse POST Requests
+   */
 
-//     //Logs  GET / -  105.112.226.145
+  //  app.use(bodyParser.urlencoded({ extended: false }));
 
-//   app.use("/public", (req, res, next) => {
-//    console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
-//     next();
-//   });
+  //  app.use(bodyParser.json());
 
-//   //Logs    GET /public/style.css -  105.112.226.145
+  /**
+   * Implementing a Root-Level Request Logger Middleware
+   * The Middleware must be placed before the HTTP verbs, or
+   * HTTP functions
+   */
+  // app.use("/", (req, res, next) => {
+  //     console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
+  //     next();
+  //   });
 
-//   app.use("/json", (req, res, next) => {
-//     console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
-//     next();
-//   });
+  //     //Logs  GET / -  105.112.226.145
 
-//   //Logs     GET /style.css -  105.112.226.145
+  //   app.use("/public", (req, res, next) => {
+  //    console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
+  //     next();
+  //   });
 
-/**
- * Chain Middleware to Create a Time Server
- * ES6 Code version
- */
-// app.get('/now', (req, res, next) => {
-//     req.time = new Date().toString();  // Hypothetical synchronous operation
-//     next();
-//   }, (req, res) => {
-//     res.json({time: req.time});
-//   });
+  //   //Logs    GET /public/style.css -  105.112.226.145
 
-/**
- * Get Route Parameter Input from the Client
- */
-//  app.get('/:word/echo', (req, res) => {
-//     const word = req.params.word;
-//     res.send({echo: word});
-// });
+  //   app.use("/json", (req, res, next) => {
+  //     console.log(req.method + " " + req.path + " " + "-" + " ", req.ip);
+  //     next();
+  //   });
 
-module.exports = app;
+  //   //Logs     GET /style.css -  105.112.226.145
+
+  /**
+   * Chain Middleware to Create a Time Server
+   * ES6 Code version
+   */
+  // app.get('/now', (req, res, next) => {
+  //     req.time = new Date().toString();  // Hypothetical synchronous operation
+  //     next();
+  //   }, (req, res) => {
+  //     res.json({time: req.time});
+  //   });
+
+  /**
+   * Get Route Parameter Input from the Client
+   */
+  //  app.get('/:word/echo', (req, res) => {
+  //     const word = req.params.word;
+  //     res.send({echo: word});
+  // });
+
+  /**
+   * Get Query Parameter Input from the Client
+   *
+   */
+  //  app.get("/name", (req, res) => {
+  //     let firstName = req.query.first;
+  //     let lastName = req.query.last;
+
+  //     res.json({ name: `${firstName} ${lastName}` });
+  //   });
+
+  /**
+   * Get Data from POST Requests
+   *
+   */
+  // app.post('/name', (req, res) => {
+  //     let firstName = req.body.first;
+  //     let lastName = req.body.last;
+
+  //    res.json({ name: `${firstName} ${lastName}` });
+  //  }),
+
+  (module.exports = app);
